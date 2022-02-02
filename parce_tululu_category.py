befb433b5fc1f120099113123bc9909b834c8d3a
learn2410 <learn2410@mail.ru>
2022-02-02 19:56:25 +0500

    improved code

--- parce_tululu_category.py
+++ parce_tululu_category.py
@@ -3,6 +3,7 @@ import json
 import os
 import sys
 from urllib.parse import urlparse
+from time import sleep
 
 import requests
 from bs4 import BeautifulSoup
@@ -10,12 +11,11 @@ from lxml import etree
 from pathvalidate import sanitize_filename
 from tqdm import tqdm
 
-ROOT_URL = "https://tululu.org"
-TARGET_RUBRIC_URL = "/l55/"
-LIB_DIR = 'library/'
-TEXTS_SUBDIR = 'books/'
-IMAGES_SUBDIR = 'images/'
-JSON_PATH = ''
+ROOT_URL = 'https://tululu.org'
+TARGET_RUBRIC_URL = '/l55/'
+LIB_DIR = 'library'
+TEXTS_SUBDIR = 'books'
+IMAGES_SUBDIR = 'images'
 
 
 def check_for_redirect(response):
@@ -37,7 +37,7 @@ def download_file(url, filepath):
 
 
 def parse_book_page(response):
-    selector = ".tabs #content"
+    selector = '.tabs #content'
     content_block = BeautifulSoup(response.content, 'lxml').select(selector)
     dom = etree.HTML(str(content_block))
     return {
@@ -52,16 +52,12 @@ def parse_book_page(response):
 
 
 def parse_rubric_limits(response):
-    dom = etree.HTML(str(response.content))
-    current = ''.join(dom.xpath('//*/div[@id="content"]/*/span[@class="npage_select"]/b/text()'))
-    maxnum = ''.join(dom.xpath('//*/div[@id="content"]/*/a[@class="npage"]/text()')[-1:])
-    max_page_num = max(int(current), int(maxnum)) if current.isdigit() and maxnum.isdigit() else 1
-    books_per_page = len(dom.xpath('//*/div[@id="content"]/*/div[@class="bookimage"]/text()'))
-    return {'max_page_num': max_page_num, 'book_per_page': books_per_page}
+    pag = ''.join(etree.HTML(str(response.content)).xpath('//*/div[@id="content"]/p[@class="center"]//text()')[-1:])
+    return int(pag) if pag.isdigit() else 1
 
 
 def parse_rubric_page(response):
-    selector = "table.tabs #content"
+    selector = 'table.tabs #content'
     content_block = BeautifulSoup(response.content, 'lxml').select(selector)
     dom = etree.HTML(str(content_block))
     book_bloks = dom.xpath('//*/table[@class="d_book"]')
@@ -70,20 +66,23 @@ def parse_rubric_page(response):
               }
              for book in book_bloks
              ]
-    return {'books': books}
+    return books
 
 
-def download_book(book_link, rewrite=False, skip_txt=False, skip_img=False):
+def download_book(book_link, txt_dir, img_dir, skip_txt=False, skip_img=False):
     book = parse_book_page(get_response(book_link))
-    if not book["text_link"]:
+    if not book['text_link']:
         return {}
-    book_filepath = os.path.normcase(os.path.join(LIB_DIR, TEXTS_SUBDIR, sanitize_filename(f'{book["title"]}.txt')))
-    if not skip_txt and (not os.path.exists(book_filepath) or not rewrite):
-        download_file(f'{ROOT_URL}{book["text_link"]}', book_filepath)
-    img_filename = urlparse(ROOT_URL + book['img_link']).path.split('/')[-1]
-    img_filepath = os.path.normcase(os.path.join(LIB_DIR, IMAGES_SUBDIR, sanitize_filename(f'{img_filename}')))
-    if not skip_img and (not os.path.exists(img_filepath) or not rewrite):
-        download_file(f'{ROOT_URL}{book["img_link"]}', img_filepath)
+    book_url = f'{ROOT_URL}{book["text_link"]}'
+    book_filename = sanitize_filename(f'{book["title"]}.txt')
+    book_filepath = os.path.normcase(os.path.join(txt_dir, book_filename))
+    if not skip_txt:
+        download_file(book_url, book_filepath)
+    img_url = f'{ROOT_URL}{book["img_link"]}'
+    img_filename = sanitize_filename(urlparse(img_url).path.split('/')[-1])
+    img_filepath = os.path.normcase(os.path.join(img_dir, img_filename))
+    if not skip_img:
+        download_file(img_url, img_filepath)
     return {book['book_link']: {
         'title': book['title'],
         'autor': book['autor'],
@@ -94,75 +93,65 @@ def download_book(book_link, rewrite=False, skip_txt=False, skip_img=False):
     }}
 
 
-parser = argparse.ArgumentParser()
-
-
-def arg_dest_folder(path):
-    global LIB_DIR
-    path = os.path.normcase(path)
-    if LIB_DIR != path:
-        LIB_DIR = path
-
-
-def arg_json_path(path):
-    global JSON_PATH
-    if os.path.splitext(path)[1] != '.json':
-        parser.error("тип файла длжен быть .json")
-    path = os.path.normcase(path)
-    os.makedirs(os.path.dirname(path), exist_ok=True)
-    JSON_PATH = path
-    return path
-
-
-def parser_addargs():
+def main():
+    url = f'{ROOT_URL}{TARGET_RUBRIC_URL}'
+    limit = parse_rubric_limits(get_response(url))
+    parser = argparse.ArgumentParser()
     parser.add_argument('--start_page', type=int, default=1)
     parser.add_argument('--end_page', type=int, default=9999)
     parser.add_argument('--skip_imgs', action='store_true')
     parser.add_argument('--skip_txt', action='store_true')
-    parser.add_argument('--dest_folder', type=arg_dest_folder)
-    parser.add_argument('--json_path', type=arg_json_path)
-
-
-def main():
-    global JSON_PATH
-    url = f'{ROOT_URL}{TARGET_RUBRIC_URL}'
-    limits = parse_rubric_limits(get_response(url))
-    parser_addargs()
+    parser.add_argument('--dest_folder')
+    parser.add_argument('--json_path')
     args = parser.parse_args(sys.argv[1:])
-    if args.end_page > limits['max_page_num']:
-        args.end_page = limits['max_page_num']
-    if args.end_page <= args.start_page:
-        args.end_page = args.start_page
-        print(f'скачивание книг со страницы {args.start_page}')
+    args.end_page = max(args.start_page, min(limit, args.end_page))
+    print(f'скачивание книг со страниц от: {args.start_page} до {args.end_page}')
+    root_dir = os.path.normcase(args.dest_folder) if args.dest_folder else LIB_DIR
+    txt_dir = os.path.join(root_dir, TEXTS_SUBDIR)
+    img_dir = os.path.join(root_dir, IMAGES_SUBDIR)
+    if args.json_path:
+        json_path = os.path.normcase(args.json_path)
+        if os.path.splitext(json_path)[1] != '.json':
+            json_path = json_path + '.json'
     else:
-        print(f'скачивание книг со страниц от {args.start_page} до {args.end_page}')
-    os.makedirs(os.path.join(LIB_DIR, TEXTS_SUBDIR), exist_ok=True)
-    os.makedirs(os.path.join(LIB_DIR, IMAGES_SUBDIR), exist_ok=True)
-    if not JSON_PATH:
-        JSON_PATH = os.path.join(LIB_DIR, 'catalog.json')
-    if os.path.exists(JSON_PATH) and os.path.isfile(JSON_PATH):
-        with open(JSON_PATH, 'r') as file:
+        json_path = os.path.join(root_dir, 'catalog.json')
+    os.makedirs(img_dir, exist_ok=True)
+    os.makedirs(txt_dir, exist_ok=True)
+    os.makedirs(os.path.dirname(json_path), exist_ok=True)
+    if os.path.exists(json_path) and os.path.isfile(json_path):
+        with open(json_path, 'r') as file:
             catalog = json.load(file)
     else:
         catalog = {}
-    start_page = args.start_page
-    end_page = args.end_page
-    progressbar = tqdm(total=(end_page - start_page + 1) * 25)
-    for page in range(start_page, end_page + 1):
-        response = get_response(f'{url}{page}')
-        for book in parse_rubric_page(response)['books']:
-            try:
-                catalog.update(
-                    download_book(f'{ROOT_URL}{book["book_link"]}',
-                                  skip_txt=args.skip_txt,
-                                  skip_img=args.skip_imgs)
-                )
-            except requests.exceptions.HTTPError:
-                pass
-            progressbar.update(1)
+    print('--- составляю список книг со страниц')
+    progressbar = tqdm(total=(args.end_page - args.start_page + 1))
+    aims = []
+    for page in range(args.start_page, args.end_page + 1):
+        progressbar.update(1)
+        try:
+            books = parse_rubric_page(get_response(f'{url}{page}'))
+            aims.extend(books)
+        except requests.exceptions.HTTPError:
+            pass
+    progressbar.close()
+    print('--- скачиваю книги')
+    progressbar = tqdm(total=len(aims))
+    for book in aims:
+        progressbar.update(1)
+        try:
+            catalog.update(
+                download_book(f'{ROOT_URL}{book["book_link"]}',
+                              txt_dir,
+                              img_dir,
+                              skip_txt=args.skip_txt,
+                              skip_img=args.skip_imgs
+                              )
+            )
+        except requests.exceptions.HTTPError:
+            pass
     progressbar.close()
     if len(catalog) > 0:
-        with open(JSON_PATH, 'w') as file:
+        with open(json_path, 'w') as file:
             json.dump(catalog, file, ensure_ascii=False, indent=2)
     print('*** работа завершена ***')
 
